fb145ee3cb8bd46f617d746a666e6f1f7808e3a5
sdelmoralg <sdelmoral@byui.edu>
2025-01-12 22:48:03 -0500

    completed prep3 - Loops - guess my number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        /*Console.Write("What is the magic number? ");
+        string userResponse = Console.ReadLine();
+        int magicNumber = int.Parse(userResponse);*/
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = 0;
+
+        while (magicNumber != guess)
+        {
+            Console.Write("What is your guess? ");
+            string userGuess = Console.ReadLine();
+            guess = int.Parse(userGuess);
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else if  (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }
+
+        
     }
 }
