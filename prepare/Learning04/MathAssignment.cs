0e9a5e4066057bed6532c18e2c28962e5ce0b9b2
sdelmoralg <sdelmoral@byui.edu>
2025-02-17 22:04:23 -0500

    Finished Learning Activity 04 - Assignments

---
+++ prepare/Learning04/MathAssignment.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class MathAssignment : Assignment
+{
+    private string _textBookSection;
+    private string _problems;
+
+    public MathAssignment(string studentName, string topic, string textBookSection, string problems) : base(studentName, topic)
+    {
+        _textBookSection = textBookSection;
+        _problems = problems;
+    }
+
+    public string GetHomeworkList()
+    {
+        //section, and problems
+        return _textBookSection + " " + _problems;
+    }
+
+}
+
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(a1.GetSumary());
+        Console.WriteLine();
+
+        MathAssignment a2 = new MathAssignment("Roberto Rodriguez", "Fractions", "Section 7.3", "Problems 8-19");
+        Console.WriteLine(a2.GetSumary());
+        Console.WriteLine(a2.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment a3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(a3.GetSumary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
