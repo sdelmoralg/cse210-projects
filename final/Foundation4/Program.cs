9b3548a1feb912981a9b1a45817d4e0942d89091
sdelmoralg <sdelmoral@byui.edu>
2025-04-02 23:38:03 -0400

    added a list of activities in program

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,24 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("\n------------------------------------\n");
+        List<Activity> activities = new List<Activity>
+        {
+            new Running(new DateTime(2025, 03, 02), 90, 5),
+            new Cycling(new DateTime(2025, 02, 23), 126, 16),
+            new Swimming(new DateTime(2025, 11, 03), 60, 30)
+        };
 
-        Activity swimming = new Swimming(new DateTime(2025, 11, 03), 60, 30);
-        Console.WriteLine(swimming.GetSummary());
-       
-        Console.WriteLine("\n------------------------------------\n");
+        foreach (var activity in activities)
+        {
+            Console.WriteLine($"{activity.GetSummary()}\n");
+        }
 
-        Activity running = new Running(new DateTime(2025, 03, 02), 90, 5);
-        Console.WriteLine(running.GetSummary());
-
-        Console.WriteLine("\n------------------------------------\n");
-
-        Activity cycling = new Cycling(new DateTime(2025, 02, 23), 126, 16);
-        Console.WriteLine(cycling.GetSummary());
-
-        Console.WriteLine("\n------------------------------------\n");
-
-        // to do - Put each of these activities in the same list. Then iterate through this
+        // Put each of these activities in the same list. Then iterate through this
         // list and call the GetSummary method on each item and display the results.
 
 
--- final/Foundation4/Running.cs
+++ final/Foundation4/Running.cs
@@ -28,8 +28,6 @@ public class Running : Activity
 
     public override string GetSummary()
     {
-        // get summary in the from: date - Activity - distance - speed-pace
-        // 03 Nov 2022 Running (30 min): Distance 4.8 km, Speed: 9.6 kph, Pace: 6.25 min per km
         return base.GetSummary();
     }
 }
